a2e9e83414d8935ef67b844d5198c214a6736f21
Teamaddoners <teamaddoners@gmail.com>
2024-08-20 17:53:42 +0530

    Team Addoners Ultimate Optimizers
    used for better shader performance

---
+++ src/main/java/teamaddoners/Teamaddonersultimateoptimizers.java
@@ -0,0 +1,12 @@
+package teamaddoners;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class Teamaddonersultimateoptimizers implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
